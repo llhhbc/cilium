41d3b5ee53cd629ab0edfe2b1d42faf0288ec5f9
longhui.li <longhui.li@woqutech.com>
2024-02-28 10:43:09 +0800

    【qfl-00】add build

--- build.sh
+++ build.sh
@@ -4,9 +4,5 @@ set -ex
 revision=`git rev-parse HEAD`
 
 docker buildx build --label commit=$revision -t registry.woqutech.com/woqutech/cilium-cilium:v1.12.7-hcs-oem -f ./images/cilium/Dockerfile . --push
-
-docker pull registry.woqutech.com/woqutech/cilium-cilium:v1.12.7-hcs-oem
-docker tag registry.woqutech.com/woqutech/cilium-cilium:v1.12.7-hcs-oem registry.woqutech.com/woqutech/cilium-cilium:v1.12.7-ipam-vpc-hcs-oem
-
-docker push registry.woqutech.com/woqutech/cilium-cilium:v1.12.7-ipam-vpc-hcs-oem
+docker buildx build --label commit=$revision -t registry.woqutech.com/woqutech/cilium-cilium:v1.12.7-ipam-vpc-hcs-oem -f ./images/cilium/Dockerfile . --push
 
